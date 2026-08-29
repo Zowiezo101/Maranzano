2ba922b8103296b8bce48e3390741a80ef89a44d
Zowiezo101 <Zowiezo101@users.noreply.github.com>
2026-08-29 16:17:51 +0200

    Almost fully working homepage

--- website/www/index.php
+++ website/www/index.php
@@ -21,13 +21,12 @@
 
         <title>Mafiani</title>
     </head>
-
-    <body class="d-flex flex-column mx-auto bg-body fst-italic">
+    
+    <body class="d-flex flex-column min-vh-100 mx-auto bg-body fst-italic">
         <!-- Imports -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
-
+        <script src="https://code.jquery.com/jquery-4.0.0.min.js" integrity="sha256-OaVG6prZf4v69dPg6PhVattBXkcOWQB62pdZ3ORyrao=" crossorigin="anonymous"></script>
+  
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-6 mx-auto px-0">
@@ -47,18 +46,15 @@
                                 <b>Menu</b>
                             </div>
                             <div class="row">
-                                * Login<br/>
-                                * Sign up<br/>
-                                * Rules<br/>
-                                * About us<br/><br/>
+                                <button type="button" class="btn btn-link text-start" onclick="onClickHome()">Home</button>
+                                <button type="button" class="btn btn-link text-start" data-bs-toggle="modal" data-bs-target="#loginModal">Log in</button>
+                                <button type="button" class="btn btn-link text-start" data-bs-toggle="modal" data-bs-target="#registerModal">Sign up</button>
+                                <button type="button" class="btn btn-link text-start" onclick="onClickRules()">Rules</button>
+                                <button type="button" class="btn btn-link text-start" onclick="onClickAboutUs()">About us</button>
                             </div>
                         </div>
-                        <div class="col-6">
-                            <!-- Welcome message -->
-                            <b>Welcome to Mafiani</b>
-                            <p>An oldskool mafia game inspired by the beloved "DeLuccio". Climb to power in this thrilling player driven mafia experience. Where you choose how to play and who to trust.</p>
-                            <br/>
-                            <p>-Team Mafiani</p>
+                        <div id="content" class="col-6">
+                            <!-- Content -->
                         </div>
                         <div class="col-3">
                             <!-- News -->
@@ -86,4 +82,70 @@
                 <b>Copyright</b>
         </footer>
     </body>
+    
+    <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title fs-5" id="loginModalLabel">Logging in</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Login</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="modal fade" id="registerModal" tabindex="-1" aria-labelledby="registerModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title fs-5" id="registerModalLabel">Signing up</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Sign up</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </html>
+
+<script>
+    function onClickHome() {
+        
+        var text = `
+            <b>Welcome to Mafiani</b>
+            <p>An oldskool mafia game inspired by the beloved "DeLuccio". Climb to power in this thrilling player driven mafia experience. Where you choose how to play and who to trust.</p>
+            <br/>
+            <p>-Team Mafiani</p>`;
+                
+        $("#content").html(text);
+    }
+    
+    function onClickRules() {
+        var text = `Hier komen de regels`;
+                
+        $("#content").html(text);
+    }
+    
+    function onClickAboutUs() {
+        var text = `Hier komt een about us`;
+                
+        $("#content").html(text);
+    }
+    
+    $(function() {
+        // Show the welcome message
+        onClickHome();
+    });
+</script>
