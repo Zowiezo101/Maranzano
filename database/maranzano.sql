d5abaa624feeb3d5f994d82c68d437c76ada6156
Zowiezo101 <Zowiezo101@users.noreply.github.com>
2026-09-02 22:57:14 +0200

    Updated database backup with verified accounts

--- database/maranzano.sql
+++ database/maranzano.sql
@@ -51,8 +51,8 @@ TRUNCATE TABLE `users`;
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `pass_hash`, `token`, `is_verified`, `type`, `created_at`) VALUES
-(1, 'Zowiezo101', 'zoegeurts@gmail.com', '$2y$10$JsXQfQQXT4BL1FVkETbRpO/DyaW4aHuKrpFEw3ceeZhKpnOE4cDVi', 'a7c52a3cca3c515ad50c062da782bef81d6ec530aa8d033894bb53e9c7d554e343a12350556c2fde1ddbd116b97dd6352915', 0, 0, '2026-09-01 13:36:42'),
-(2, 'TheToweler', 'wilcovdb17@gmail.com', '$2y$10$pMT8mTu1KUaWzVuaODGD2O2VHGNWk2Jr/cmeaHLrPo2JCpCytS6oS', '5aaafe35f3ea4e7f7818bb1f9b81d621b2d8960f0fc7fc4a21bf819da8eecc33a7afae68877b4728bc1462db5b48ca614435', 0, 0, '2026-09-01 13:44:03');
+(1, 'Zowiezo101', 'zoegeurts@gmail.com', '$2y$10$JsXQfQQXT4BL1FVkETbRpO/DyaW4aHuKrpFEw3ceeZhKpnOE4cDVi', NULL, 1, 0, '2026-09-01 13:36:42'),
+(2, 'TheToweler', 'wilcovdb17@gmail.com', '$2y$10$pMT8mTu1KUaWzVuaODGD2O2VHGNWk2Jr/cmeaHLrPo2JCpCytS6oS', NULL, 1, 0, '2026-09-01 13:44:03');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
