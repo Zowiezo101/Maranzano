1df66af241b706283070e2cc96b2de90f5eb0b05
Zowiezo101 <Zowiezo101@users.noreply.github.com>
2026-09-11 16:35:52 +0200

    Database has been updated

--- database/maranzano.sql
+++ database/maranzano.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: database:3306
--- Gegenereerd op: 01 sep 2026 om 13:50
+-- Gegenereerd op: 11 sep 2026 om 14:29
 -- Serverversie: 8.3.0
 -- PHP-versie: 8.3.33
 
@@ -26,15 +26,49 @@ USE `maranzano`;
 -- --------------------------------------------------------
 
 --
+-- Tabelstructuur voor tabel `login_user`
+--
+
+DROP TABLE IF EXISTS `login_user`;
+CREATE TABLE IF NOT EXISTS `login_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `expires_at` timestamp NOT NULL,
+  `used` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `reset_pass`
+--
+
+DROP TABLE IF EXISTS `reset_pass`;
+CREATE TABLE IF NOT EXISTS `reset_pass` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `expires_at` timestamp NOT NULL,
+  `used` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Tabelstructuur voor tabel `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `pass_hash` varchar(255) NOT NULL,
-  `token` varchar(255) DEFAULT NULL,
   `is_verified` tinyint NOT NULL DEFAULT '0',
   `type` int NOT NULL DEFAULT '0',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -42,17 +76,29 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Tabel leegmaken voor invoegen `users`
---
-
-TRUNCATE TABLE `users`;
---
 -- Gegevens worden geëxporteerd voor tabel `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `pass_hash`, `token`, `is_verified`, `type`, `created_at`) VALUES
-(1, 'Zowiezo101', 'zoegeurts@gmail.com', '$2y$10$JsXQfQQXT4BL1FVkETbRpO/DyaW4aHuKrpFEw3ceeZhKpnOE4cDVi', NULL, 1, 0, '2026-09-01 13:36:42'),
-(2, 'TheToweler', 'wilcovdb17@gmail.com', '$2y$10$pMT8mTu1KUaWzVuaODGD2O2VHGNWk2Jr/cmeaHLrPo2JCpCytS6oS', NULL, 1, 0, '2026-09-01 13:44:03');
+INSERT INTO `users` (`id`, `name`, `email`, `pass_hash`, `is_verified`, `type`, `created_at`) VALUES
+(1, 'Zowiezo101', 'zoegeurts@gmail.com', '$2y$10$JsXQfQQXT4BL1FVkETbRpO/DyaW4aHuKrpFEw3ceeZhKpnOE4cDVi', 1, 0, '2026-09-01 13:36:42'),
+(2, 'TheToweler', 'wilcovdb17@gmail.com', '$2y$10$pMT8mTu1KUaWzVuaODGD2O2VHGNWk2Jr/cmeaHLrPo2JCpCytS6oS', 1, 0, '2026-09-01 13:44:03');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `verify_user`
+--
+
+DROP TABLE IF EXISTS `verify_user`;
+CREATE TABLE IF NOT EXISTS `verify_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `expires_at` timestamp NOT NULL,
+  `used` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
